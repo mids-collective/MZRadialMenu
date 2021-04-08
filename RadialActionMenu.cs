b25397f82e35bdcd6f7645f17a1ce754b706b447
Minizbot2012 <bradleyfwin@gmail.com>
2021-04-07 21:43:13 -0400

    Fix namespace

--- RadialActionMenu.cs
+++ RadialActionMenu.cs
@@ -2,7 +2,7 @@ using System;
 using System.Runtime.InteropServices;
 using System.Text;
 using Dalamud.Plugin;
-using ImGuiComponents;
+using ImComponents;
 using ImGuiNET;
 using MZRadialMenu.Attributes;
 
