2b0c34bb551f2bb4f8b8cac1d8b3576f5f7c5e8c
Minizbot2012 <bradleyfwin@gmail.com>
2024-02-16 09:50:19 -0500

    Fix HQ, maybe?

--- Services/ItemService.cs
+++ Services/ItemService.cs
@@ -8,16 +8,13 @@ public unsafe sealed class ItemService : IService<ItemService>
 {
     public static ItemService Instance => Service<ItemService>.Instance;
     private const int aetherCompassID = 2001886;
-    private delegate void UseItemDelegate(nint agent, uint cnt, uint itm, uint unk1, short unk2);
-    private UseItemDelegate useItem;
-    private delegate uint GetActionID(uint a, uint b);
-    private GetActionID getActionID;
+    private static delegate* unmanaged<nint, uint, uint, uint, short, void> useItem;
     private Dictionary<uint, string> usables = new();
     private nint itemContextMenuAgent;
     private ItemService()
     {
-        useItem = Marshal.GetDelegateForFunctionPointer<UseItemDelegate>(DalamudApi.SigScanner.ScanText(SigService.GetSig("UseItem")));
-        getActionID = Marshal.GetDelegateForFunctionPointer<GetActionID>(DalamudApi.SigScanner.ScanText(SigService.GetSig("GetActionID")));
+        useItem = (delegate* unmanaged<nint, uint, uint, uint, short, void>)DalamudApi.SigScanner.ScanText(SigService.GetSig("UseItem"));
+        usables.Clear();
         InitUsables();
     }
 
@@ -30,24 +27,23 @@ public unsafe sealed class ItemService : IService<ItemService>
         var useHQ = !newName.Equals(name);
         newName = newName.ToLower().Trim(' ');
 
-        UseItem(usables.First(i => i.Value == newName).Key + (uint)(useHQ ? 1_000_000 : 0));
+        UseItem(usables.First(x => x.Value == newName).Key + (uint)(useHQ ? 1_000_000 : 0));
     }
 
     private void UseItem(uint id)
     {
-        if (id == 0 || !usables.ContainsKey(id is >= 1_000_000 and < 2_000_000 ? id - 1_000_000 : id)) return;
+        if (id == 0) return;
         // Aether Compass
         if (id == aetherCompassID)
         {
             ActionManager.Instance()->UseAction(ActionType.Action, 26988);
         }
-        else if (usables[id] == "wondrous tails")
+        else if (id == usables.First(x => x.Value == "wondrous tails").Key)
         {
             ActionManager.Instance()->UseAction(ActionType.KeyItem, id);
         }
         else
         {
-            id = getActionID(2, id);
             useItem(itemContextMenuAgent, id, 9999, 0, 0);
         }
     }
@@ -57,7 +53,7 @@ public unsafe sealed class ItemService : IService<ItemService>
         usables = DalamudApi.GameData.GetExcelSheet<Lumina.Excel.GeneratedSheets.Item>()!.Where(i => i.ItemAction.Row > 0).ToDictionary(i => i.RowId, i => i.Name.ToString().ToLower())
             .Concat(DalamudApi.GameData.GetExcelSheet<Lumina.Excel.GeneratedSheets.EventItem>()!.Where(i => i.Action.Row > 0).ToDictionary(i => i.RowId, i => i.Name.ToString().ToLower()))
             .ToDictionary(kv => kv.Key, kv => kv.Value);
-        usables[aetherCompassID] = DalamudApi.GameData.GetExcelSheet<Lumina.Excel.GeneratedSheets.EventItem>()!.GetRow(aetherCompassID)?.Name.ToString().ToLower()!;
+        usables[aetherCompassID] = "aether compass";
         itemContextMenuAgent = UIService.Instance.GetAgentByInternalID(AgentId.InventoryContext);
     }
     public void Dispose()
